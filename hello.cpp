b2b1696899058e33d0db70316d94e0a0bdf6cc4c
Tanaf <tanaf.dev@gmail.com>
2020-12-03 16:51:34 +0600

    A new function added named display

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+void display(string name)
+{
+    cout << name << ", Welcome to Github" << endl;
+}
 int main()
 {
+    string name;
     cout << "Hello Github" << endl;
+    name = "Rakib Hasan";
+    display(name);
     getch();
 }
